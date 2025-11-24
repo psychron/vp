a0bdbc018ad3e440813ca201450cfcff282562bf
psychron <psychron7@hotmail.com>
2025-11-24 17:21:46 +0800

    Update Index.tsx with logo

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import ContactForm from "@/components/ContactForm";
 import Footer from "@/components/Footer";
+import logo from "@/logo.png";
 
 const Index = () => {
   return (
