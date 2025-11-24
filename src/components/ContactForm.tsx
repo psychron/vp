e58b585260313cc71c6c8087f9e99fd4eda38071
psychron <psychron7@hotmail.com>
2025-11-24 12:49:37 +0800

    Added debugging

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -52,11 +52,15 @@ const ContactForm = () => {
 
     try {
       // Google Apps Script submission
-      const response = await fetch(https://script.google.com/macros/s/AKfycbycn9k6-H2gTf-BT4sYkPJQlGvVJJ0s8w1eRLP4YzBfVlogbyWFNFzUg8NtuBuEejo/exec, {
+      const response = await fetch("https://script.google.com/macros/s/AKfycbycn9k6-H2gTf-BT4sYkPJQlGvVJJ0s8w1eRLP4YzBfVlogbyWFNFzUg8NtuBuEejo/exec", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
+
+      // Logging result for debugging
+      const result = await response.json();
+      console.log(result);
 
       if (response.ok) {
         toast({
